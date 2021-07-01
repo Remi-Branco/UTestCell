d5a5d05746cdff920ff1493ba5875ffc90d1e117
Remi-Branco <remi.branco@hotmail.fr>
2021-07-01 15:46:55 +1000

    Update utest.py

--- utest.py
+++ utest.py
@@ -10,7 +10,11 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(square(4), 16)
         with  self.assertRaises(TypeError):
             square('') #need to be in a context manager for the test to fail and be accepted as such
-    
+
+    def test_n_letters_is_5(self):
+        self.assertEqual(n_letters_is_5('Remi'),False)
+        self.assertEqual(n_letters_is_5('Maz'), False)
+        self.assertEqual(n_letters_is_5('abcde'), True)
             
 if __name__ == '__main__':
     unittest.main()
